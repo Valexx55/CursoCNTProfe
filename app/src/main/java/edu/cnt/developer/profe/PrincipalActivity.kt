c2d1df861151a0445c17a478799e2be78c920f68
valexx55 <valexx55@gmail.com>
2025-05-12 20:30:35 +0200

    intent implicitos webview

---
+++ app/src/main/java/edu/cnt/developer/profe/PrincipalActivity.kt
@@ -0,0 +1,27 @@
+package edu.cnt.developer.profe
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_principal)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val urlRemota:String = "https://cntg.xunta.gal/"
+        val intentImplicito = Intent(Intent.ACTION_VIEW, Uri.parse(urlRemota))
+        //TODO REVISAR ESTE CÓDIGO Y LOS INTENTS COMUNES DOC OFICIAL
+        startActivity(intentImplicito)
+    }
+}
