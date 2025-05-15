7607fef3ffa3cf21982bfa46ce3dc115658b8b20
valexx55 <valexx55@gmail.com>
2025-05-15 17:22:54 +0200

    progressbar frameyout

--- app/src/main/java/edu/cnt/developer/profe/productos/ProductosActivity.kt
+++ app/src/main/java/edu/cnt/developer/profe/productos/ProductosActivity.kt
@@ -2,6 +2,8 @@ package edu.cnt.developer.profe.productos
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import edu.cnt.developer.profe.R
 import edu.cnt.developer.profe.RedUtil
+import edu.cnt.developer.profe.databinding.ActivityProductosBinding
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -17,10 +20,12 @@ class ProductosActivity : AppCompatActivity() {
 
     lateinit var productoService: ProductoService
     lateinit var listadoProductos: ListadoProductos
+    lateinit var binding: ActivityProductosBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_productos)
+        binding = ActivityProductosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://my-json-server.typicode.com/")
@@ -36,20 +41,28 @@ class ProductosActivity : AppCompatActivity() {
             lifecycleScope.launch {
                 Log.d("MIAPP", "En corrutina 1")
                 listadoProductos = productoService.obtenerProductos()
+                binding.barraProgreso.visibility = View.INVISIBLE//hacemos que la barra de progreso se haga invisible después de conseguir los productos
                 Log.d("MIAPP", "En corrutina 2 TAMAÑO ${listadoProductos.size}")
                 listadoProductos.forEach{
                     producto -> Log.d("MIAPP", "PRODUCTO $producto")
                 }
-                //TODO: ARMAR EL RECYCLER, EL ADAPTER, EL LAYAOUT MANAGER, PARA MOSTRAR LA LISTA
-                //PROGRESSBAR
-                //FRAMELAYOUT
+
+                mostrarListaProductos()
             }
             Log.d("MIAPP", "Fuera de corrutina")
 
 
         } else {
             //TOAST informativo
+            binding.barraProgreso.visibility = View.INVISIBLE//hacemos que la barra de progreso se haga invisible después de conseguir los productos
+            Toast.makeText(this, "SIN ACCESO A INTERNET", Toast.LENGTH_LONG).show()
         }
 
     }
+
+    fun mostrarListaProductos ()
+    {
+        //TODO: ARMAR EL RECYCLER, EL ADAPTER, EL LAYAOUT MANAGER, PARA MOSTRAR LA LISTA
+    }
+
 }
